af26549517fa016c45bf596c2c12ccc01aa370fe
reb89xx-design <reb.89xx@gmail.com>
2025-12-12 19:53:13 +0100

    Update add_format_page.py

--- pages/add_format_page.py
+++ pages/add_format_page.py
@@ -1,6 +1,9 @@
+# all'inizio del file pages/home_page.py
 import streamlit as st
-import mock_store as store
+from utils.auth import require_login
+require_login()
 
+# poi il resto del file come prima
 def add_format_page():
     st.header("Aggiungi Format")
     name = st.text_input("Nome format")
