2e0d82e64edc07a64498b92cbd87b4601d8be417
reb89xx-design <reb.89xx@gmail.com>
2025-12-12 19:53:04 +0100

    Update add_event_page.py

--- pages/add_event_page.py
+++ pages/add_event_page.py
@@ -1,6 +1,9 @@
+# all'inizio del file pages/home_page.py
 import streamlit as st
-from utils.events_utils import event_form
+from utils.auth import require_login
+require_login()
 
+# poi il resto del file come prima
 def add_event_page():
     st.header("Aggiungi Evento")
     event_form()
