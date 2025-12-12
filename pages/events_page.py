b5f6367e667f0a26cbe54f87e84dbace524d76a3
reb89xx-design <reb.89xx@gmail.com>
2025-12-12 19:55:31 +0100

    Update events_page.py

--- pages/events_page.py
+++ pages/events_page.py
@@ -1,4 +1,9 @@
+# all'inizio del file pages/home_page.py
 import streamlit as st
+from utils.auth import require_login
+require_login()
+
+# poi il resto del file come prima
 from utils.events_utils import events_table, event_form
 
 def events_page():
